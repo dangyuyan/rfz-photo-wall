c6971995f12bdfb23b8788bbed22a1c3ba175684
dangyuyan <2020212063001@cuc.edu.cn>
2026-04-27 22:15:19 +0800

    Revert "photo size"
    This reverts commit b265dd7ffce240888edbed937f4a6bcdec23f06b.

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -5,9 +5,6 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL?: string
   readonly VITE_SUPABASE_ANON_KEY?: string
   readonly VITE_SUPABASE_BUCKET?: string
-  readonly NEXT_PUBLIC_SUPABASE_URL?: string
-  readonly NEXT_PUBLIC_SUPABASE_ANON_KEY?: string
-  readonly SUPABASE_BUCKET?: string
 }
 
 interface ImportMeta {
